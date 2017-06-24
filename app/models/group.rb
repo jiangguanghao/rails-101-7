bc5100cfd139eda9ac1f8706e30095a3b36f771e
Your Name <you@example.com>
2017-06-24 11:33:56 +0800

    implement my posts

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,5 +5,5 @@ class Group < ApplicationRecord
   validates :title, presence: true
 
   has_many :group_relationships
-  has_many :mebmbers, through: :group_relationships, source: :user
+  has_many :members, through: :group_relationships, source: :user
 end
